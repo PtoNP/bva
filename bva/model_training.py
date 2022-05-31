79ac77eab30ce8ae20ae93c331058bfd85f11127
Theo Panel <theopanel@outlook.fr>
2022-05-31 15:40:10 +0200

    draft training model

---
+++ bva/model_training.py
@@ -0,0 +1,15 @@
+from preprocess import get_all_videos_sequences_by_window
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import RMSprop
+
+
+
+if __name__ == "__main__":
